56289db2f0d5b3d209b7a104848cd52e6574f025
Alex <lesha6481078@outlook.com>
2020-12-28 20:33:34 +0300

    Added chat functions

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import json, vk, VKsettings, random
 
 app = Flask(__name__)
 
@@ -8,9 +9,22 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/helperBot/api', methods=['POST'])
+@app.route('/helperBot/api/', methods=['POST'])
 def processing():
-    return '173a66e7'
+    data = json.loads(request.data)
+    if 'type' not in data.keys():
+        return 'not vk'
+    elif data['type'] == 'confirmation':
+        return VKsettings.confirmation_token
+    elif data['type'] == 'message_new':
+        session = vk.Session()
+        api = vk.API(session, v=VKsettings.api_ver)
+        user_id = data['object']['message']['from_id']
+        api.messages.send(access_token=VKsettings.token, user_id=str(user_id), message='Hi, I\'m new bot!',
+                          random_id=random.getrandbits(64))
+        return 'ok'
+
+
 
 
 if __name__ == '__main__':
